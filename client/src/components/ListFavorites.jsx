9155c38e6e6125dcf31e094054bba8d3cb27a595
BigPhatNerd <wilsonhorrell@gmail.com>
2024-06-29 09:18:46 -0500

    Error message in favorites

--- client/src/components/ListFavorites.jsx
+++ client/src/components/ListFavorites.jsx
@@ -1,20 +1,27 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { UserContext } from '../contexts/UserContext'
 
 function ListFavorites() {
   const { favorites, getWeather, setWeather } = useContext(UserContext)
+  const [errorMessage, setErrorMessage] = useState('')
 
   const handleFavoriteClick = async (fav) => {
     try {
       const weatherData = await getWeather(fav)
       setWeather(weatherData)
+      setErrorMessage('')
     } catch (error) {
+      setErrorMessage(
+        error.response ? error.response.data.message : error.message
+      )
+      setTimeout(() => setErrorMessage(''), 3000)
       console.error(error)
     }
   }
   return (
     <div>
       <h3>Favorites</h3>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
       <ul>
         {favorites &&
           favorites.map((fav, index) => {
